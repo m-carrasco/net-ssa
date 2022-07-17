60eedb6133234940def0c488a800e0d7f264a27c
Manuel Carrasco <m.carrasco@imperial.ac.uk>
2022-07-17 18:08:09 +0100

    Fix mono version.

--- ci/install-mono.sh
+++ ci/install-mono.sh
@@ -2,5 +2,5 @@
 apt-get update && apt-get install -y \
     gnupg ca-certificates
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | tee /etc/apt/sources.list.d/mono-official-stable.list
+echo "deb https://download.mono-project.com/repo/ubuntu stable-focal/snapshots/6.12.0.182 main" | tee /etc/apt/sources.list.d/mono-official-stable.list
 apt-get update && apt-get install -y mono-devel mono-utils
