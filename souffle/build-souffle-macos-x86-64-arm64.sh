1c450733acb9b0749791706bd18b114093d40929
Manuel Carrasco <m.carrasco@imperial.ac.uk>
2022-07-24 10:44:55 +0100

    More work.

---
+++ souffle/build-souffle-macos-x86-64-arm64.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+cpp_file=$(mktemp -u).cpp
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+include_dir=$parent_path/src
+souffle_file=$include_dir/ssa-query.dl
+target_os="macos-x86-64-arm64"
+output_dir=$parent_path/bin/$target_os
+output_bin=$output_dir/ssa-query-$target_os
+
+echo "script path: "$parent_path
+echo "soufle include dir:" $include_dir
+echo "target souffle script:" $souffle_file
+echo "temp cpp file:" $cpp_file
+echo "output dir:" $output_dir
+echo "output bin:" $output_bin
+echo $(souffle --version)
+
+souffle --generate=$cpp_file --include-dir=$include_dir $souffle_file
+mkdir -p $output_dir
+$CXX -O3 --std=c++17 -static -target x86_64-apple-darwin-macho -target arm64-apple-darwin -o $output_bin.both $cpp_file
+$CXX -O3 --std=c++17 -static -target arm64-apple-darwin -o $output_bin.arm64 $cpp_file 
+$CXX -O3 --std=c++17 -static -target x86_64-apple-darwin-macho -o $output_bin.x86_64 $cpp_file
+echo "Done"
